e93905ddf1d400a85c77a45d0955a5e91bb7ce68
alveena <alveenafaisal72@gmail.com>
2023-02-25 12:45:29 +0100

    deployig App

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/health_tracker" element={<Home />} />
         <Route path="Main">
           <Route index element={<Main />} />
           <Route path=":recordId" element={<Edit />} />
